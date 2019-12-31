ac6c879180aa8425421788be9d8ba9c9cd721fcb
morsi <Mor.Elkayam@biu.ac.il>
2019-12-31 21:41:39 +0200

    compare

--- compare_rn.sh
+++ compare_rn.sh
@@ -7,10 +7,8 @@ aux="${@:4}"
 
 echo algorithm=$algorithm dim=$dim index=$index
 
-args="--best-explore-update --algorithm=$algorithm --game=CMP --budget=800 --replay-memory-factor=80 --warmup-minibatch=1 --n-explore=1024 --batch=1024 --learn-iteration=20"
+args="--best-explore-update --algorithm=$algorithm --game=CMP --budget=1500 --replay-memory-factor=80 --warmup-minibatch=1 --n-explore=1024 --batch=1024 --learn-iteration=20"
 
-CUDA_VISIBLE_DEVICES=0, python main.py --agent=trust --identifier=trust --action-space=$dim --problem-index=$index $args $aux &
-CUDA_VISIBLE_DEVICES=1, python main.py --agent=robust --identifier=robust --action-space=$dim --problem-index=$index $args $aux &
-CUDA_VISIBLE_DEVICES=2, python main.py --agent=single --identifier=single --action-space=$dim --problem-index=$index $args $aux &
+CUDA_VISIBLE_DEVICES=1, python main.py  --identifier=debug --action-space=$dim --problem-index=$index $args $aux &
 
 
